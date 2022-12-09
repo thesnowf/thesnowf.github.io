0780af4f5bd8b7dfe8ffbacfbd74c31a43fb1444
thesnowf <aa232239@gmail.com>
2022-12-09 16:01:21 +0800

    thesnowfff
    123

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+let myHeading = document.querySelector("h1");
+myHeading.textContent = "千變萬化之賈克修";
+
+function toggle_info() {
+  const foo = document.getElementById("foo");
+  foo.classList.toggle("invisible");
+}
